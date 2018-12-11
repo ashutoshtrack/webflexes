0e2d7de8e529cac73eab203b1fbea4849492827e
Ashutosh Telang <ashutoshtel@gmail.com>
2018-12-11 23:24:40 +0530

    Updates

---
+++ precache-manifest.237d48586019eeea9d3427a784cdef14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/webflexes/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "319cc863fb6c05dbf9a6",
+    "url": "/webflexes/static/js/runtime~main.319cc863.js"
+  },
+  {
+    "revision": "bcdff555f6c378487892",
+    "url": "/webflexes/static/js/main.bcdff555.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/webflexes/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "bcdff555f6c378487892",
+    "url": "/webflexes/static/css/main.b7483b81.chunk.css"
+  },
+  {
+    "revision": "839cbf166cba6993a4be8601c02ab2e4",
+    "url": "/webflexes/index.html"
+  }
+];
